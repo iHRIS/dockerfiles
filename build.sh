6a28b36bf085c2bddef27c2cb60191c8b2a222f7
Richard Stanley <richardlstanley@gmail.com>
2017-10-31 16:48:21 -0400

    push to separate manage docker hub

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-docker rmi ihris/ihris:4.3-dev_manage-demo_0.1.0 || true
+docker rmi ihris/manage:4.3-dev-demo-0.1.0 || true
 
 docker build \
 --build-arg VER=4.3-dev \
@@ -10,11 +10,11 @@ docker build \
 --build-arg TYPE=Demo \
 --build-arg SITE=manage-demo \
 --build-arg MCONFIG=iHRIS-Manage-Demo.xml \
--t ihris/ihris:4.3-dev_manage-demo_0.1.0 .
+-t ihris/manage:4.3-dev-demo-0.1.0 .
 
-docker push ihris/ihris:4.3-dev_manage-demo_0.1.0
+docker push ihris/manage:4.3-dev-demo-0.1.0
 
-docker rmi ihris/ihris:4.3-dev_manage_0.1.0 || true
+docker rmi ihris/manage:4.3-dev-0.1.0 || true
 
 docker build \
 --build-arg VER=4.3-dev \
@@ -23,6 +23,6 @@ docker build \
 --build-arg TYPE=blank \
 --build-arg SITE=manage \
 --build-arg MCONFIG=iHRIS-Manage-BLANK.xml \
--t ihris/ihris:4.3-dev_manage_0.1.0 .
+-t ihris/manage:4.3-dev-0.1.0 .
 
-docker push ihris/ihris:4.3-dev_manage_0.1.0
+docker push ihris/manage:4.3-dev-0.1.0
