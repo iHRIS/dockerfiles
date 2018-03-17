78dc8bda623e510dcc146559e262afb3daa2397f
Richard Stanley <richardlstanley@gmail.com>
2018-03-17 18:01:57 -0400

    more dev builds, incl fpm

--- build.sh
+++ build.sh
@@ -1,26 +1,52 @@
 #!/usr/bin/env bash
 set -ex
 
-docker rmi ihris/manage:4.3-demo-0.2.0 || true
+# release version blank
+docker rmi ihris/manage:4.3-0.2.0 || true
+docker build -f Dockerfile -t ihris/manage:4.3-0.2.0 .
+docker push ihris/manage:4.3-0.2.0
 
-docker build \
+# release version demo
+docker rmi ihris/manage:4.3-demo-0.2.0 || true
+docker build -f Dockerfile \
 --build-arg MVER=4.3 \
 --build-arg SOFT=ihris-manage \
 --build-arg TYPE=Demo \
 --build-arg SITE=manage-demo \
 --build-arg MCONFIG=iHRIS-Manage-Demo.xml \
 -t ihris/manage:4.3-demo-0.2.0 .
-
 docker push ihris/manage:4.3-demo-0.2.0
 
-docker rmi ihris/manage:4.3-0.2.0 || true
+# dev version blank
+docker rmi ihris/manage:4.3-dev-0.2.0 || true
+docker build -f Dockerfile-dev -t ihris/manage:4.3-dev-0.2.0 .
+docker push ihris/manage:4.3-dev-0.2.0
 
-docker build \
+# release version demo
+docker rmi ihris/manage:4.3-dev-demo-0.2.0 || true
+docker build -f Dockerfile-dev \
 --build-arg MVER=4.3 \
 --build-arg SOFT=ihris-manage \
---build-arg TYPE=blank \
---build-arg SITE=manage \
---build-arg MCONFIG=iHRIS-Manage-BLANK.xml \
--t ihris/manage:4.3-0.2.0 .
+--build-arg TYPE=Demo \
+--build-arg SITE=manage-demo \
+--build-arg MCONFIG=iHRIS-Manage-Demo.xml \
+-t ihris/manage:4.3-dev-demo-0.2.0 .
+docker push ihris/manage:4.3-dev-demo-0.2.0
 
-docker push ihris/manage:4.3-0.2.0
+# fpm dev version blank
+docker rmi ihris/manage:4.3-dev-fpm-0.2.0 || true
+docker build -f Dockerfile-fpm-dev -t ihris/manage:4.3-dev-fpm-0.2.0 .
+docker push ihris/manage:4.3-dev-fpm-0.2.0
+
+# fpm release version demo
+docker rmi ihris/manage:4.3-dev-fpm-demo-0.2.0 || true
+docker build -f Dockerfile-dev \
+--build-arg MVER=4.3 \
+--build-arg SOFT=ihris-manage \
+--build-arg TYPE=Demo \
+--build-arg SITE=manage-demo \
+--build-arg MCONFIG=iHRIS-Manage-Demo.xml \
+-t ihris/manage:4.3-dev-fpm-demo-0.2.0 .
+docker push ihris/manage:4.3-dev-fpm-demo-0.2.0
+
+# there is no release version for fpm yet, blank or demo
